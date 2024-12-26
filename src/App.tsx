65df5a8b3d7a9283297125bbc88c0f38f9536b55
caio <caiothalles607@gmail.com>
2024-12-26 18:53:57 -0300

    feat: created Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { Global } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
       <Global />
-    </div>
+    </>
   );
 }
 
