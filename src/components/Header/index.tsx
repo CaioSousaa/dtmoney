5632c4de2e3569057dc08d05652bd1de8a4796ba
caio <caiothalles607@gmail.com>
2024-12-30 11:32:47 -0300

    feat: create new modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,18 @@
 import { Container, Content } from "./styles";
-import { useState } from "react";
-import logoImg from "../../assets/logo.svg";
-import Modal from "react-modal";
+import logoImg from "../../assetss/logo.svg";
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
-    useState(false);
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false);
-  }
-
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button" onClick={handleOpenNewTransactionModal}>
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
-
-        <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
       </Content>
       yarn add @types/react@18.0.22
     </Container>
